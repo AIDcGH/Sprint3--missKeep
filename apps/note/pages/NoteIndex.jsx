a42e9b921536e2fe4eda4b641183b5002052a62a
AIDcGH <aviv.c000@gmail.com>
2025-04-20 18:13:44 +0300

    added basic notes service

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,11 @@
+import NotePreview from '../cmps/NotePreview.jsx'
 
-export function NoteIndex() {
-    return <div>note app</div>
+export function NoteIndex({ notes }) {
+  return (
+    <section className="note-index">
+      {notes.map((note) => (
+        <NotePreview key={note.id} note={note} />
+      ))}
+    </section>
+  )
 }
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,42 @@
-// note service
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+const NOTE_KEY = 'noteDB'
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save
+}
+
+window.bs = noteService
+
+function query(filterBy = {}) {
+    return storageService.query(NOTE_KEY)
+        .then(notes => {
+            // console.log('notes:', notes)
+
+            if (filterBy.title) {
+                const regExp = new RegExp(filterBy.title, 'i')
+                notes = notes.filter(note => regExp.test(note.title))
+            }
+            if (filterBy.minPrice) {
+                notes = notes.filter(note => note.listPrice.amount >= filterBy.minPrice)
+            }
+            return notes
+        })
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) return storageService.put(NOTE_KEY, note)
+    else return storageService.post(NOTE_KEY, note)
+}
