dae07fb01c1699a7f31f4afef2009339fecdd49a
AIDcGH <aviv.c000@gmail.com>
2025-04-22 21:39:00 +0300

    added an internal note creation function

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -47,40 +47,29 @@ function _createNotes() {
 
     if (notes && notes.length) return
 
-    notes = [
-        {
-            id: 'n101',
-            createdAt: 1112222,
-            type: 'NoteTxt',
-            isPinned: true,
-            style: { backgroundColor: '#00d' },
-            info: { txt: 'Fullstack Me Baby!' }
-        },
-        {
-            id: 'n102',
-            createdAt: 1112223,
-            type: 'NoteImg',
-            isPinned: false,
-            info: {
-                url: 'http://some-img/me',
-                title: 'Bobi and Me'
-            },
-            style: { backgroundColor: '#00d' }
-        },
-        {
-            id: 'n103',
-            createdAt: 1112224,
-            type: 'NoteTodos',
-            isPinned: false,
-            info: {
-                title: 'Get my stuff together',
-                todos: [
-                    { txt: 'Driving license', doneAt: null },
-                    { txt: 'Coding power', doneAt: 187111111 }
-                ]
-            }
-        }
-    ]
+    notes.push(_createNote('NoteTxt', { txt: 'Fullstack Me Baby!' },
+        { backgroundColor: '#00d' }, 'pin', 1112221, 'dummyNote1'))
+
+    notes.push(_createNote('NoteImg', { url: 'http://some-img/me', title: 'Bobi and Me' },
+        { backgroundColor: '#08d' }, 'basic', 1112222, 'dummyNote2'))
+
+    notes.push(_createNote('NoteTodos', {
+        title: 'Get my stuff together', todos: [
+            { txt: 'Driving license', doneAt: null },
+            { txt: 'Coding power', doneAt: 187111111 }
+        ]}, undefined, 'bin', 1112223, 'dummyNote3'))
 
     utilService.saveToStorage(NOTE_KEY, notes)
+}
+
+function _createNote(type, info, style = { backgroundColor: '#bbb' },
+    placement = 'basic', createdAt = Date.now(), id = utilService.makeId()) {
+    return {
+        id,
+        createdAt,
+        type,
+        placement,
+        info,
+        style
+    }
 }
